aa705b967179cfe6db1ed552d1f5446f47da5870
suh4rev <artem0219@mail.ru>
2025-06-10 01:58:32 +0500

    process data - select columns and fill NaN

--- lab4/scripts/process_data.py
+++ lab4/scripts/process_data.py
@@ -1,18 +1,16 @@
+# lab4/scripts/process_data.py
 import pandas as pd
 import os
 
 data_path = 'lab4/data/titanic.csv'
 
-if not os.path.exists(data_path):
-    print(f"Файл не найден по пути: {data_path}")
-else:
-    # Загружаем датасет
-    df = pd.read_csv(data_path)
+# Загружаем датасет
+df = pd.read_csv(data_path)
 
-    # Выбираем только необходимые колонки
-    df_processed = df[['Pclass', 'Sex', 'Age']].copy()
+# Выбираем только необходимые колонки: Pclass, Sex, Age
+df_processed = df[['Pclass', 'Sex', 'Age']].copy()
 
-    # Перезаписываем
-    df_processed.to_csv(data_path, index=False)
+# Перезаписываем исходный файл обработанными данными
+df_processed.to_csv(data_path, index=False)
 
-    print(f"Датасет обработан. В файле '{data_path}' оставлены только колонки Pclass, Sex, Age.")
+print(f"Датасет обработан. В файле '{data_path}' оставлены только колонки Pclass, Sex, Age.")
